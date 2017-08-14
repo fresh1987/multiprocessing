72a30606919b8b780e874bd157db339afb9a9ae6
fresh <fresh@debian8>
2017-08-14 23:37:36 +0300

    small change

---
+++ 35_time_comparison.py
@@ -0,0 +1,49 @@
+
+from multiprocessing import Pool, Process
+from time import time, sleep
+
+def funct(a):
+    for k in range(1000):
+        for j in range(10000):
+            a = j*(j+1)
+    return a
+
+
+if __name__ == "__main__":
+    t1 = time()
+    pool = Pool()
+    m4 = pool.map(funct,range(12))
+    print(time() - t1)
+
+    t2 = time()
+    list_of_process = []
+    for i in range(12):
+        list_of_process.append(Process(target=funct, args=(i,)))
+    for i in range(12):
+        list_of_process[i].start()
+    for i in range(12):
+        list_of_process[i].join()
+    print(time() - t2)
+
+    t3 = time()
+    for i in range(12):
+        funct(i)
+    print (time() - t3)
+
+
+    t4 = time()
+    list_of_process = []
+    for i in range(12):
+        list_of_process.append(Process(target=funct, args=(i,)))
+    for i in range(12):
+        list_of_process[i].start()
+        list_of_process[i].join()
+    print(time() - t4)
+
+    t5 = time()
+    list_of_process = []
+    for i in range(12):
+        list_of_process.append(Process(target=funct, args=(i,)))
+        list_of_process[i].start()
+        list_of_process[i].join()
+    print(time() - t5)
